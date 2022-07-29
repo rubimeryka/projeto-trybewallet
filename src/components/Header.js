26284ec2f825ea0dbe1e6196f8036b1502f9ab23
rubimeryka <erykarubim13@gmail.com>
2022-07-29 19:06:46 -0300

    requisitos 1 e 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <div>
+          <p data-testid="email-field">{ email }</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </div>
+      </header>
     );
   }
 }
+
+const mapStateToProps = (store) => ({
+  email: store.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -2,10 +2,7 @@
 import { WALLET_INPUT } from '../actions';
 
 const INITIAL_STATE = {
-  currencies: [], // array de string
-  expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
-  editor: false, // valor booleano que indica de uma despesa está sendo editada
-  idToEdit: 0, // valor numérico que armazena o id da despesa que esta sendo editada
+
 };// definir estado inicial da carteira
 
 const walletReducer = (state = INITIAL_STATE, action) => {
